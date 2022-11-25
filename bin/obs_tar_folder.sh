b3785fb62ae442f01a0be3cc8e4828a416938751
astrokatross <kathryn.ross@icrar.org>
2022-11-25 15:30:38 +0800

    ⚡️ change to srun for pawsey

--- bin/obs_tar_folder.sh
+++ bin/obs_tar_folder.sh
@@ -113,7 +113,7 @@ chmod 755 "${script}"
 
 # sbatch submissions need to start with a shebang
 echo '#!/bin/bash' > ${script}.sbatch
-echo "singularity run ${GXCONTAINER} ${script}" >> ${script}.sbatch
+echo "srun ${GXSRUNLINE} singularity run ${GXCONTAINER} ${script}" >> ${script}.sbatch
 
 if [ ! -z ${GXNCPULINE} ]
 then
